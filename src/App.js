48f97b53837fcae20aaf5e2b39aaa05e663c6719
Dave Dehghani <dave.dehghani@rakuten.com>
2018-12-16 12:55:08 -0500

    grabbed all article instances from Contentful and rendered out all their fields

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import contentfulCMS from './utils/cms'
 
 class App extends Component {
+  state = {
+    articles: []
+  }
+
+  componentDidMount() {
+    return this.articles = contentfulCMS.getEntries().then(data => {
+      this.setState({
+        articles: data.items
+      }) 
+    });
+  }
+
+  renderArticleData() {
+    return this.state.articles.map((article, index) => {
+      const { 
+        articleAuthor, 
+        articleBodyText, 
+        articleSummary, 
+        articleTitle, 
+        publishedDate, 
+        photos 
+      } = article.fields;
+
+      return (
+        <div key={`${articleTitle}=${index}`}>
+          <div>Title: {articleTitle}</div>
+          <div>Author: {articleAuthor}</div>
+          <div>Summary: {articleSummary}</div>
+          <div>Date: {publishedDate}</div>
+          <div>Article Text: {articleBodyText}</div>
+          <br />
+          <br />
+          { 
+            photos ? 
+            photos.map(photo => <img key={photo.sys.id} width={250} height={250} src={photo.fields.file.url} alt={photo.fields.file.title} />) :
+            null
+          }
+        </div>
+      )
+    })
+  }
+
   render() {
+    
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+            <div>
+              {this.renderArticleData()}
+            </div>
         </header>
       </div>
     );
